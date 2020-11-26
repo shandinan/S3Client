d06d6f542d33b185b209ed31216c3b8531ef5ac8
shandinan <shandinan@live.com>
2020-11-26 17:04:36 +0800

    sdn

--- src/main/java/com/star/main.java
+++ src/main/java/com/star/main.java
@@ -11,6 +11,11 @@ import com.amazonaws.ClientConfiguration;
 import com.amazonaws.Protocol;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.policy.Policy;
+import com.amazonaws.auth.policy.Principal;
+import com.amazonaws.auth.policy.Resource;
+import com.amazonaws.auth.policy.Statement;
+import com.amazonaws.auth.policy.actions.S3Actions;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
@@ -57,7 +62,26 @@ public class main {
             objects = s3Client.listNextBatchOfObjects(objects);
         } while (objects.isTruncated());
 
+        //    SetS3Web("new-bucket-6d4e39ce","index.html",null);
+        //配置当前存储桶为web类型
+        // BucketWebsiteConfiguration website_config = new BucketWebsiteConfiguration("index.html");
+        //  s3Client.setBucketWebsiteConfiguration("new-bucket-6d4e39ce", website_config);
+        //获取当前存储桶web的列表
+        BucketWebsiteConfiguration config = s3Client.getBucketWebsiteConfiguration("new-bucket-6d4e39ce");
+        System.out.format("Index document: %s\n", config.getIndexDocumentSuffix());
 
+        //删除存储桶wen配置
+        // s3Client.deleteBucketWebsiteConfiguration("new-bucket-6d4e39ce");
+
+        Statement policy = new Statement(Statement.Effect.Allow)
+                .withPrincipals(Principal.AllUsers)
+                .withActions(S3Actions.GetObject)
+                .withResources(new Resource("arn:aws:s3:::" + "new-bucket-6d4e39ce" + "/*"));
+
+        //获取存储桶测试
+        BucketPolicy bucket_policy = s3Client.getBucketPolicy("new-bucket-6d4e39ce");
+        String policy_text = bucket_policy.getPolicyText();
+        System.out.println("存储桶安全策略为：" + policy_text);
         //文件上传
 /*        try {
             s3Client.putObject("bucketname", "keyname", new File("your file path"));
@@ -90,32 +114,35 @@ public class main {
         }*/
 
 
-        //配置存储桶为网站
-
-
-        }
-
-        public void SetS3Web(String bucket_name, String index_doc, String error_doc) {
-            BucketWebsiteConfiguration website_config = null;
-
-            if (index_doc == null) {
-                website_config = new BucketWebsiteConfiguration();
-            } else if (error_doc == null) {
-                website_config = new BucketWebsiteConfiguration(index_doc);
-            } else {
-                website_config = new BucketWebsiteConfiguration(index_doc, error_doc);
-            }
-
-            final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
-            try {
-                s3.setBucketWebsiteConfiguration(bucket_name, website_config);
-            } catch (AmazonServiceException e) {
-                System.out.format(
-                        "Failed to set website configuration for bucket '%s'!\n",
-                        bucket_name);
-                System.err.println(e.getErrorMessage());
-                System.exit(1);
-            }
-        }
-
     }
+
+    /***
+     * 配置 S3 存储桶 为web站点形式
+     * @param bucket_name
+     * @param index_doc
+     * @param error_doc
+     */
+    public static void SetS3Web(String bucket_name, String index_doc, String error_doc) {
+        BucketWebsiteConfiguration website_config = null;
+
+        if (index_doc == null) {
+            website_config = new BucketWebsiteConfiguration();
+        } else if (error_doc == null) {
+            website_config = new BucketWebsiteConfiguration(index_doc);
+        } else {
+            website_config = new BucketWebsiteConfiguration(index_doc, error_doc);
+        }
+
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        try {
+            s3.setBucketWebsiteConfiguration(bucket_name, website_config);
+        } catch (AmazonServiceException e) {
+            System.out.format(
+                    "Failed to set website configuration for bucket '%s'!\n",
+                    bucket_name);
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+    }
+
+}
